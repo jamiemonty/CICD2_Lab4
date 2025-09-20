bbb01348fb59b0c5dd56d1abf32be68f180923c0
jamiemonty <G00419525@atu.ie>
2025-09-21 00:43:17 +0100

    Added code for updating an existing users information

--- app/main.py
+++ app/main.py
@@ -23,7 +23,25 @@ def add_user(user: User):
     users.append(user)
     return user
 
+@app.put("/api/users/{user_id}", status_code=status.HTTP_200_OK)
+def update_user(user_id: int, updated_user: User):
+    for i, u in enumerate(users):
+        if u.user_id == user_id:
+            users[i] = updated_user
+            return updated_user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with ID {user_id} not found")
+
 @app.get("/hello")
 def hello():
     return {"message": "Hello World!"}
 
+#Where do validation errors (422) come from in FastAPI + Pydantic?
+#Validation errors come from pydantic automatically validating the request body sent, if variable is different it causes an error.
+
+#Why is returning 201 for create (POST) and 404 for missing resources important?
+#Because 201 is the official code for create, tells the client the POST req worked. Same with 404, it is the official code for 
+#resource not found (valid request but something is not there)
+
+#How will this project layout help when we add a database and tests later?
+#The current layout has all the necessary app logic, schemas, database code, and tests organised and able
+#for swapping.
