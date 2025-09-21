8391f77c84d880409d114ee164b6b1d9a440586d
jamiemonty <G00419525@atu.ie>
2025-09-21 14:46:02 +0100

    Added code to follow a certain pattern with student_id, done this by using a pattern with regex.

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,10 @@
 # app/schemas.py
-from pydantic import BaseModel, EmailStr, constr, conint
+from typing import Annotated
+from pydantic import BaseModel, EmailStr, constr, conint, Field
 
 class User(BaseModel):
     user_id: int
+    student_id: Annotated[str, Field(pattern=r"^S\d{7}$")]
     name: constr(min_length=2, max_length=50)
     email: EmailStr
     age: conint(gt=18)
